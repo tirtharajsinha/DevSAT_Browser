3b073f20a594b60a7106e0f2a99cd86d69ec59fd
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2021-05-18 23:29:45 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+options = {"build_exe": {"includes": "atexit"}}
+
+executables = [Executable("devsat.py", base=base)]
+
+setup(
+    name="browser",
+    version="1.0.1.4 LTS",
+    description="Sample cx_Freeze PyQt5 script",
+    options=options,
+    executables=executables,
+)
+
+# python setup.py build
