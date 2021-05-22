42abdb4c3a88dbe82d401637ee214c33a3aba940
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2021-05-22 20:18:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,20 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-options = {"build_exe": {"includes": "atexit"}}
+options = {"build_exe": {"includes": "atexit", 'include_files': ["static/"]}}
 
 executables = [Executable("devsat.py", base=base)]
 
 setup(
     name="browser",
-    version="1.0.1.4 LTS",
+    version="1.0.2.1 LTS",
     description="Sample cx_Freeze PyQt5 script",
     options=options,
     executables=executables,
 )
+
+# python setup.py build
+
 # instruction to get .exe
 
 # 1) install cs_freeze with  "pip install cx-Freeze"
